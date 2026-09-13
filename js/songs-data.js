30fc569af944cf7854c39bf3df05124296206450
legacybiblechurch <legacybiblechurchadmin@gmail.com>
2026-09-13 11:22:15 -0700

    worship studio: update lyrics for By Faith
    Edited in Worship Studio by hand.

--- js/songs-data.js
+++ js/songs-data.js
@@ -285,10 +285,16 @@ const LEGACY_SONGS = {
     { label: "", lines: ["In the lives of those who prove", "His faithfulness", "Who walk by faith and not by sight"] },
     { label: "", lines: ["By faith our", "Fathers roamed the earth", "With the pow'r of", "His promise in their hearts"] },
     { label: "", lines: ["Of a holy city built by", "God's own hand", "A place where peace and justice reign"] },
-    { label: "", lines: ["We will stand as children of the promise", "We will fix our eyes on Him, our soul's reward", "Till the race is finished", "And the work is done", "We'll walk by faith and not by sight", "By faith the", "Prophets saw a day", "When the longed-for", "Messiah would appear"] },
+    { label: "", lines: ["We will stand as children of the promise", "We will fix our eyes on Him, our soul's reward", "Till the race is finished", "And the work is done", "We'll walk by faith and not by sight"] },
+    { label: "", lines: ["By faith the", "Prophets saw a day", "When the longed-for Messiah would appear"] },
     { label: "", lines: ["With the pow'r to Break the chains of", "Sin and death", "And rise triumphant from the grave"] },
-    { label: "", lines: ["We will stand as children of the promise", "We will fix our eyes on Him our soul's reward", "Till the race is finished", "And the work is done", "We'll walk by faith and not by sight"] },
-    { label: "", lines: ["We will stand as children of the promise", "We will fix our eyes on Him our soul's reward", "Till the race is finished", "And the work is done", "We'll walk by faith and not by sight", "We'll walk by faith and not by sight"] }
+    { label: "", lines: ["By Faith the Church was called to go", "In the power of the Spirit to the lost", "To deliver captives and to preach good news", "In every corner of the earth"] },
+    { label: "", lines: ["We will stand as children of the promise", "We will fix our eyes on Him our soul's reward", "Till the race is finished", "And the work is done"] },
+    { label: "", lines: ["We'll walk by faith and not by sight"] },
+    { label: "", lines: ["By faith this mountain shall be moved", "And the power of the gospel shall prevail", "For we know in Christ all things are possible", "For all who call upon His name"] },
+    { label: "", lines: ["We will stand as children of the promise", "We will fix our eyes on Him our soul's reward", "Till the race is finished", "And the work is done", "We'll walk by faith and not by sight", "We'll walk by faith and not by sight"] },
+    { label: "", lines: ["We will stand as children of the promise", "We will fix our eyes on Him our soul's reward", "Till the race is finished and the work is done", "We'll walk by faith and not by sight"] },
+    { label: "", lines: ["We'll walk by faith and not by sight"] }
   ]
   },
   "by-grace-alone": {
