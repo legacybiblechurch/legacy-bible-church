bfd04a34fb05fbd1479634a061febe8d7c51a272
legacybiblechurch <legacybiblechurchadmin@gmail.com>
2026-09-20 09:39:01 -0700

    worship studio: update Christ, the Sure and Steady Anchor

--- js/songs-data.js
+++ js/songs-data.js
@@ -398,7 +398,7 @@ const LEGACY_SONGS = {
     { label: "", lines: ["In the suffering, in the sorrow", "When my sinking hopes are few", "I will hold fast to the anchor", "It shall never be removed"] },
     { label: "", lines: ["Christ the sure and steady anchor", "While the tempest rages on", "When temptation claims the battle", "And it seems the night has won"] },
     { label: "", lines: ["Deeper still, then, goes the anchor", "Though I justly stand accused", "I will hold fast to the anchor", "It shall never be removed"] },
-    { label: "", lines: ["Christ the sure and steady anchor", "Through the floods of unbelief", "Hopeless, somehow, O my soul, now", "Lift your eyes to Calvary", "This my ballast of assurance", "See His love forever proved", "All my hope is in the anchor", "It shall never be removed"] },
+    { label: "", lines: ["Christ the sure and steady anchor", "Through the floods of unbelief", "Hopeless, somehow, O my soul, now", "Lift your eyes to Calvary", "This my ballast of assurance", "See His love forever proved", "I will hold fast to the anchor", "It shall never be removed"] },
     { label: "", lines: ["Christ the sure and steady anchor", "As we face the wave of death", "When these trials give way to glory", "As we draw our final breath"] },
     { label: "", lines: ["We will cross that great horizon", "Clouds behind and life secured", "And the calm will be the better", "For the storms that we endured"] },
     { label: "", lines: ["Christ the shore of our salvation", "Ever faithful, ever true", "We will hold fast to the anchor", "It shall never be removed"] }
